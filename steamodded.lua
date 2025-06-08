cea0dedcd25df0ef13cdc1b6753e1131628cbcb7
Xhack <72313282+energykid@users.noreply.github.com>
2025-06-08 09:30:55 -0400

    Slight cleanup

--- steamodded.lua
+++ steamodded.lua
@@ -1,16 +1,3 @@
---- STEAMODDED HEADER
---- MOD_NAME: My Menagerie
---- MOD_ID: MyMenagerie
---- MOD_AUTHOR: ENNWAY
---- MOD_DESCRIPTION: My masterpiece
-
---- PRIORITY: 0
---- DISPLAY_NAME: Menagerie
---- PREFIX: mng
---- VERSION: 1.0.0
-----------------------------------------------
-------------MOD CODE -------------------------
-
 if SMODS and SMODS.current_mod then
 
 	SMODS.Atlas({
@@ -21,6 +8,3 @@ if SMODS and SMODS.current_mod then
 	})
 
 end
-
-----------------------------------------------
-------------MOD CODE END----------------------
